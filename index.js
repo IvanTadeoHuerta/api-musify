fe673b9eb104ac4e9cbe97d8be53bdb5565f5a82
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-09-13 16:50:00 -0500

    Command debug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://localhost:27017/curso_mean2',(err,res)=>{
     if(err){
         throw err;
     }else{
-        console.log('La conexion a la base de datos esta funcionando');
+        // console.log('La conexion a la base de datos esta funcionando');
         app.listen(port, function(){
             console.log("Servidor de api rest de musica escuchando en http:localhost:"+port)
         })
